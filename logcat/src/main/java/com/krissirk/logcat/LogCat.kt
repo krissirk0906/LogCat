7e2021609310cc378a4fde34c42ee0c476f44482
Cao Thành Tâm <tam.ct@gmail.com>
2022-10-24 08:54:36 +0700

    Change Toast

--- logcat/src/main/java/com/krissirk/logcat/LogCat.kt
+++ logcat/src/main/java/com/krissirk/logcat/LogCat.kt
@@ -3,7 +3,7 @@ package com.krissirk.logcat
 import android.content.Context
 import android.widget.Toast
 
-class LogCat {
+object ToasterMessage {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
