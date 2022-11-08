673e6ef3929a4fb5242a6bd3a2daa35cd618676d
Cao Thành Tâm <tam.ct@gmail.com>
2022-11-08 18:01:34 +0700

    Crash Reporter.

--- mylibrary/src/main/java/com/krissirk/mylibrary/CrashReporter.kt
+++ mylibrary/src/main/java/com/krissirk/mylibrary/CrashReporter.kt
@@ -1,14 +1,25 @@
 package com.krissirk.mylibrary
 
-import android.app.Activity
+import android.app.ActivityManager
 import android.app.Application
+import android.content.Context.ACTIVITY_SERVICE
+import android.content.Context.BATTERY_SERVICE
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.res.Configuration
+import android.os.BatteryManager
 import android.util.Log
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat.getSystemService
+import com.blankj.utilcode.util.AppUtils
+import com.blankj.utilcode.util.DeviceUtils
+import com.blankj.utilcode.util.LanguageUtils
+import com.blankj.utilcode.util.ScreenUtils
 import com.krissirk.mylibrary.data_store.DataStoreRepository
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.collectLatest
+import java.util.*
 
 
 //class Logger {
@@ -62,6 +73,146 @@ object CrashReporter {
         Log.d("showLog ", listLog.toString())
     }
 
+    fun getDeviceInfo(): String {
+        try {
+            val deviceInfo = Pair("Device Information", "")
+            val appVersionName = Pair("appVersionName", AppUtils.getAppVersionName().toString())
+            val appVersionCode = Pair("appVersionCode", AppUtils.getAppVersionCode().toString())
+            val sdkVersionName = Pair("sDKVersionName", DeviceUtils.getSDKVersionName().toString())
+            val sdkVersionCode = Pair("sdkVersionCode", DeviceUtils.getSDKVersionCode().toString())
+            val manufacturer = Pair("manufacturer", DeviceUtils.getManufacturer().toString())
+            val architectures = Pair("architectures", DeviceUtils.getModel().toString())
+            val isTablet = Pair("isTablet", DeviceUtils.isTablet().toString())
+            val isSimulator = Pair("isSimulator", DeviceUtils.isEmulator().toString())
+            val batteryPercentage = Pair("batteryPercentage", getBatteryPercentage().toString())
+            val isCharging = Pair("isCharging", isCharging().toString())
+            val orientation = Pair("orientation", getOrientation().toString())
+            val screenWidth = Pair("screenWidth", ScreenUtils.getScreenWidth().toString())
+            val screenHeight = Pair("screenHeight", ScreenUtils.getScreenHeight().toString())
+            val screenDensity = Pair("screenDensity", ScreenUtils.getScreenDensity().toString())
+            val screenDensityDpi =
+                Pair("screenDensity", ScreenUtils.getScreenDensityDpi().toString())
+            val isRooted = Pair("isRooted", DeviceUtils.isDeviceRooted().toString())
+            val systemLanguage =
+                Pair("systemLanguage", LanguageUtils.getSystemLanguage().toString())
+            val applicationLanguage =
+                Pair("applicationLanguage", LanguageUtils.getAppContextLanguage().toString())
+            val availableMemory = Pair("availableMemory", getAvailableMemory().toString())
+            val totalMemory = Pair("totalMemory", getTotalMemory().toString())
+            val mutableList = mutableListOf<Pair<String, String>>()
+            mutableList.add(deviceInfo)
+            mutableList.add(appVersionName)
+            mutableList.add(appVersionCode)
+            mutableList.add(sdkVersionName)
+            mutableList.add(sdkVersionCode)
+            mutableList.add(manufacturer)
+            mutableList.add(architectures)
+            mutableList.add(isTablet)
+            mutableList.add(isSimulator)
+            mutableList.add(batteryPercentage)
+            mutableList.add(isCharging)
+            mutableList.add(orientation)
+            mutableList.add(screenWidth)
+            mutableList.add(screenHeight)
+            mutableList.add(screenDensity)
+            mutableList.add(screenDensityDpi)
+            mutableList.add(isRooted)
+            mutableList.add(systemLanguage)
+            mutableList.add(applicationLanguage)
+            mutableList.add(availableMemory)
+            mutableList.add(totalMemory)
+            var s = ""
+            mutableList.forEach {
+                s += "${it.first} : ${it.second}\n"
+            }
+            return s
+        } catch (e: Exception) {
+            return ""
+        }
+    }
+
+    private fun getAvailableMemory(): String {
+        try {
+            val actManager =
+                application.applicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+
+            // Declaring MemoryInfo object
+            val memInfo = ActivityManager.MemoryInfo()
+
+            // Fetching the data from the ActivityManager
+            actManager.getMemoryInfo(memInfo)
+
+            // Fetching the available and total memory and converting into Giga Bytes
+            val availMemory = memInfo.availMem.toDouble() / (1024 * 1024 * 1024)
+            return String.format("%.2f", availMemory) + "GiB"
+        } catch (e: Exception) {
+            return "-"
+        }
+    }
+
+    private fun getTotalMemory(): String {
+        try {
+            val actManager =
+                application.applicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+
+            // Declaring MemoryInfo object
+            val memInfo = ActivityManager.MemoryInfo()
+
+            // Fetching the data from the ActivityManager
+            actManager.getMemoryInfo(memInfo)
+
+            // Fetching the available and total memory and converting into Giga Bytes
+            val totalMemory = memInfo.totalMem.toDouble() / (1024 * 1024 * 1024)
+            return String.format("%.2f", totalMemory) + "GiB"
+        } catch (e: Exception) {
+            return "-"
+        }
+    }
+
+    private fun getLocale(): String {
+        val current: Locale = application.applicationContext.resources.configuration.locale
+        return current.toString()
+    }
+
+    private fun getOrientation(): String {
+        return when (application.applicationContext.resources.configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                "Landscape"
+            }
+            Configuration.ORIENTATION_PORTRAIT -> {
+                "Portrait"
+            }
+            else -> {
+                "Undefined"
+            }
+        }
+    }
+
+    private fun isCharging(): Boolean {
+        try {
+            val batteryStatus: Intent? =
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+                    application.applicationContext.registerReceiver(null, ifilter)
+                }
+            // isCharging if true indicates charging is ongoing and vice-versa
+            val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+            return (status == BatteryManager.BATTERY_STATUS_CHARGING
+                    || status == BatteryManager.BATTERY_STATUS_FULL)
+        } catch (e: Exception) {
+            return false
+        }
+    }
+
+    private fun getBatteryPercentage(): String {
+        return try {
+            val bm =
+                application.applicationContext.getSystemService(BATTERY_SERVICE) as BatteryManager
+            bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY).toString()
+        } catch (e: Exception) {
+            "-"
+        }
+    }
+
     suspend fun saveLogToLocalStorage() {
         DataStoreRepository(application.applicationContext).saveLogList(getActionLog())
     }
@@ -93,6 +244,7 @@ object CrashReporter {
         {
             DataStoreRepository(application.applicationContext).readLogList().cancellable()
                 .collectLatest {
+                    Log.d("device info ", getDeviceInfo())
                     Log.d("readLogList ", it)
                     if (it.isNotBlank()) {
                         reportBugToServer()
@@ -112,7 +264,6 @@ object CrashReporter {
         {
             delay(3000L)
             clearLogOfLocalStorage()
-            Log.d("11", "Sent crash report to server successfully")
             CoroutineScope(Dispatchers.Main).launch(Dispatchers.Main + handler)
             {
                 Toast.makeText(
